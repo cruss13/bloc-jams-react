7497d95ae82fc8561d1f6bcd4a8f1ca15becbfe5
Corey Russ <corey.j.russ@gmail.com>
2018-02-20 18:01:50 -0800

    New components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <header>
-          <h1>Bloc Jams<h1>
+          <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
